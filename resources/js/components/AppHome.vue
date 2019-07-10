38bfaaf3490c243f7cb7cd17afcaefb40394b491
vrathod3010 <48536022+vrathod3010@users.noreply.github.com>
2019-07-10 15:55:30 +1000

    vue and vutify setup

---
+++ resources/js/components/AppHome.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <toolbar></toolbar>
+    </div>
+</template>
+
+<script>
+import toolbar from "./Toolbar";
+export default {
+    components: { toolbar }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
